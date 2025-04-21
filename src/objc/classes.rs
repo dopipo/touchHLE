9fb4801a4b8f6ac8baade394bfa3a36af67fbd03
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2025-04-21 09:09:41 +0200

    Update classes.rs

--- src/objc/classes.rs
+++ src/objc/classes.rs
@@ -14,7 +14,6 @@ mod class_lists;
 pub(super) use class_lists::CLASS_LISTS;
 
 use super::methods::Method;
-use super::properties::IVar;
 use super::{
     id, ivar_list_t, method_list_t, nil, objc_object, AnyHostObject, HostIMP, HostObject, ObjC,
     IMP, SEL,
@@ -43,7 +42,7 @@ pub(super) struct ClassHostObject {
     pub(super) methods: HashMap<SEL, Method>,
     /// Maps ivar name to a tuple of an offset (as pointer) and an alignment.
     /// (Alignment is used during ivar reconciliation.)
-pub(super) ivars: HashMap<String, { (IVar> u32) }>,
+    pub(super) ivars: HashMap<String, (ConstPtr<GuestUSize>, u32)>,
     /// Offset into the allocated memory for the object where the ivars of
     /// instances of this class or metaclass (respectively: normal objects or
     /// classes) should live. This is always >= the value in the superclass.
@@ -79,7 +78,7 @@ impl HostObject for FakeClass {}
 ///
 /// The name, field names and field layout are based on what Ghidra outputs.
 #[repr(C, packed)]
-#[allow(dead_code, non_camel_case_types)]
+#[allow(dead_code)]
 struct class_t {
     isa: Class, // note that this matches objc_object
     superclass: Class,
@@ -93,7 +92,7 @@ unsafe impl SafeRead for class_t {}
 ///
 /// The name, field names and field layout are based on what Ghidra's output.
 #[repr(C, packed)]
-#[allow(dead_code, non_camel_case_types)]
+#[allow(dead_code)]
 struct class_rw_t {
     _flags: u32,
     instance_start: GuestUSize,
@@ -112,7 +111,6 @@ unsafe impl SafeRead for class_rw_t {}
 ///
 /// The name, field names and field layout are based on what Ghidra outputs.
 #[repr(C, packed)]
-#[allow(non_camel_case_types)]
 struct category_t {
     name: ConstPtr<u8>,
     class: Class,
