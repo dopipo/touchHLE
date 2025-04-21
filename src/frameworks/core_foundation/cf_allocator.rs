5ecd36d400345d0ba31be222443486ad97a5bf61
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2025-04-21 10:57:10 +0200

    Update cf_allocator.rs

--- src/frameworks/core_foundation/cf_allocator.rs
+++ src/frameworks/core_foundation/cf_allocator.rs
@@ -12,5 +12,51 @@ use crate::mem::Ptr;
 pub type CFAllocatorRef = CFTypeRef;
 
 pub const kCFAllocatorDefault: CFAllocatorRef = Ptr::null();
+pub const kCFAllocatorSystemDefault: CFAllocatorRef = Ptr::null();
+pub const kCFAllocatorMalloc: CFAllocatorRef = Ptr::null();
+pub const kCFAllocatorMallocZone: CFAllocatorRef = Ptr::null();
+pub const kCFAllocatorNull: CFAllocatorRef = Ptr::null();
+pub const kCFAllocatorUseContext: CFAllocatorRef = Ptr::null();
+pub const kCAFillModeRemoved: CFAllocatorRef = Ptr::null();
+pub const kCAFillModeForwards: CFAllocatorRef = Ptr::null();
+pub const kCATransitionFromBottom: CFAllocatorRef = Ptr::null();
+pub const kCATransitionFromTop: CFAllocatorRef = Ptr::null();
+pub const kCFBooleanTrue: CFAllocatorRef = Ptr::null();
+pub const kSecMatchLimit: CFAllocatorRef = Ptr::null();
+pub const kSecAttrServer: CFAllocatorRef = Ptr::null();
+pub const kSecAttrAuthenticationTypeDefault: CFAllocatorRef = Ptr::null();
+pub const kSecClassInternetPassword: CFAllocatorRef = Ptr::null();
+pub const kSecAttrAuthenticationType: CFAllocatorRef = Ptr::null();
+pub const UIEdgeInsetsZero: CFAllocatorRef = Ptr::null();
+pub const kSecMatchLimitOne: CFAllocatorRef = Ptr::null();
+pub const kSecClass: CFAllocatorRef = Ptr::null();
+pub const kSecAttrType: CFAllocatorRef = Ptr::null();
+pub const kSecValueData: CFAllocatorRef = Ptr::null();
 
-pub const CONSTANTS: ConstantExports = &[("_kCFAllocatorDefault", HostConstant::NullPtr)];
+pub const CONSTANTS: ConstantExports = &[
+    ("_kCFAllocatorDefault", HostConstant::NullPtr),
+    ("_kCFAllocatorSystemDefault", HostConstant::NullPtr),
+    ("_kCFAllocatorMalloc", HostConstant::NullPtr),
+    ("_kCFAllocatorMallocZone", HostConstant::NullPtr),
+    ("_kCFAllocatorNull", HostConstant::NullPtr),
+    ("_kCFAllocatorUseContext", HostConstant::NullPtr),
+    ("_kCAFillModeRemoved", HostConstant::NullPtr),
+    ("_kCAFillModeForwards", HostConstant::NullPtr),
+    ("_kCATransitionFromBottom", HostConstant::NullPtr),
+    ("_kCATransitionFromTop", HostConstant::NullPtr),
+    ("_kCFBooleanTrue", HostConstant::NullPtr),
+    ("_kCFTypeArrayCallBacks", HostConstant::NullPtr),
+    ("_kSecMatchLimit", HostConstant::NullPtr),
+    ("_kSecAttrServer", HostConstant::NullPtr),
+    ("_kSecAttrAuthenticationTypeDefault", HostConstant::NullPtr),
+    ("_kSecClassInternetPassword", HostConstant::NullPtr),
+    ("_kSecAttrAuthenticationType", HostConstant::NullPtr),
+    ("_UIEdgeInsetsZero", HostConstant::NullPtr),
+    ("_kSecMatchLimitOne", HostConstant::NullPtr),
+    ("_kSecAttrAuthenticationType", HostConstant::NullPtr),
+    ("_UIEdgeInsetsZero", HostConstant::NullPtr),
+    ("_kSecMatchLimitOne", HostConstant::NullPtr),
+    ("_kSecClass", HostConstant::NullPtr),
+    ("_kSecAttrType", HostConstant::NullPtr),
+    ("_kSecValueData", HostConstant::NullPtr),
+];
