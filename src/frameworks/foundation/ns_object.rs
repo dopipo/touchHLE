cc206838821d12ff756da33c32279e1f496793e0
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2025-04-21 19:53:02 +0200

    Update ns_object.rs

--- src/frameworks/foundation/ns_object.rs
+++ src/frameworks/foundation/ns_object.rs
@@ -54,6 +54,10 @@ pub const CLASSES: ClassExports = objc_classes! {
      env.objc.class_has_method(this, selector)
 }
 
++ (bool)methodForSelector:(SEL)selector {
+     env.objc.class_has_method(this, selector)
+}
+
 // See the instance method section for the normal versions of these.
 + (id)retain {
     this // classes are not refcounted
