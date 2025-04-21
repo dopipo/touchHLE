47e1d2128629f7598c468c3f7abee60e0a62f20b
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2025-04-21 11:05:34 +0200

    Update ns_url.rs

--- src/frameworks/foundation/ns_url.rs
+++ src/frameworks/foundation/ns_url.rs
@@ -6,7 +6,7 @@
 //! `NSURL`.
 
 use super::ns_string::{from_rust_string, get_static_str, to_rust_string, NSUTF8StringEncoding};
-use super::NSUInteger;
+use super::{NSInteger, NSUInteger};
 use crate::fs::{GuestPath, GuestPathBuf};
 use crate::mem::MutPtr;
 use crate::objc::{
@@ -193,6 +193,18 @@ pub const CLASSES: ClassExports = objc_classes! {
     // TODO
     nil
 }
+
++ (())setSharedURLCache:(bool)cache {
+    log!("TODO: setSharedURLCache:{}", cache);
+}
+
+- (())initWithMemoryCapacity:(NSInteger)_capacity diskCapacity:(bool)_disk diskPath:(bool)_path {
+    // TODO
+}
+
+@end
+
+@implementation NSURLProtocol: NSObject
 @end
 
 };
