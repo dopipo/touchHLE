18489e104b5909355be3b8b6d719aa25787dcd7f
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2025-04-26 09:40:46 +0200

    Update sc_network_reachability.rs

--- src/frameworks/system_configuration/sc_network_reachability.rs
+++ src/frameworks/system_configuration/sc_network_reachability.rs
@@ -44,7 +44,7 @@ fn SCNetworkReachabilityCreateWithName(
     allocator: CFAllocatorRef,
     name: ConstPtr<u8>,
 ) -> SCNetworkReachabilityRef {
-    assert_eq!(allocator, kCFAllocatorDefault); // unimplemented
+    // assert_eq!(allocator, kCFAllocatorDefault); // unimplemented
     if env
         .bundle
         .bundle_identifier()
@@ -77,7 +77,7 @@ fn SCNetworkReachabilityCreateWithAddress(
     allocator: CFAllocatorRef,
     address: ConstPtr<sockaddr>,
 ) -> SCNetworkReachabilityRef {
-    assert_eq!(allocator, kCFAllocatorDefault); // unimplemented
+    // assert_eq!(allocator, kCFAllocatorDefault); // unimplemented
     let isa = env
         .objc
         .get_known_class("_touchHLE_SCNetworkReachability", &mut env.mem);
