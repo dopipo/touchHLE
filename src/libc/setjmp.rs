340b65a530a837d11f70c83ae1f9f6905503d27f
dopipo <emssoig@gmail.com>
2025-05-01 00:17:16 +0300

    Update setjmp.rs

--- src/libc/setjmp.rs
+++ src/libc/setjmp.rs
@@ -5,9 +5,15 @@
  */
 //! `setjmp.h`.
 //!
+//! We don't have a real implementation for this right now. It could be quite
+//! tricky to write one, considering that we would need to unwind through host
+//! code, and somehow do so selectively since we have a mix of stack frames from
+//! different guest threads. For the moment, we simply pray the app never throws
+//! exceptions.
+//!
 //! Note that `setjmp` and `longjmp` are defined as macros in the C standard,
 //! but it seems like the implementation of these on iPhone OS uses real
-//! functions.
+//! functions, at least for the former.
 
 use crate::abi::GuestFunction;
 use crate::dyld::{export_c_func, FunctionExports};
@@ -30,20 +36,20 @@ struct JmpBuf {
 
 unsafe impl SafeRead for JmpBuf {}
 
+/// The signature of this is incomplete because it's a stub (see module docs).
 fn setjmp(env: &mut Environment, jmp_buf: MutPtr<JmpBuf>) -> i32 {
-    let regs = env.cpu.regs();
-    let lr = regs[crate::cpu::Cpu::LR];
-    log_dbg!("setjmp() at {:#x}", lr);
+    let lr = env.cpu.regs()[crate::cpu::Cpu::LR];
+    log_dbg!("TODO: setjmp() at {:#x}", lr);
     let buf = JmpBuf {
-        r4: regs[4],
-        r5: regs[5],
-        r6: regs[6],
-        fp: regs[abi::FRAME_POINTER],
-        r8: regs[8],
-        r10: regs[10],
-        r11: regs[11],
-        sp: regs[crate::cpu::Cpu::SP],
-        lr: regs[crate::cpu::Cpu::LR],
+        r4: env.cpu.regs()[4],
+        r5: env.cpu.regs()[5],
+        r6: env.cpu.regs()[6],
+        fp: env.cpu.regs()[abi::FRAME_POINTER],
+        r8: env.cpu.regs()[8],
+        r10: env.cpu.regs()[10],
+        r11: env.cpu.regs()[11],
+        sp: env.cpu.regs()[crate::cpu::Cpu::SP],
+        lr: env.cpu.regs()[crate::cpu::Cpu::LR],
     };
     env.mem.write(jmp_buf, buf);
     0 // no longjmp() was performed
@@ -56,23 +62,19 @@ fn longjmp(env: &mut Environment, jmp_buf: MutPtr<JmpBuf>, status: u32) {
     let buf = env.mem.read(jmp_buf);
     let cur_stack = env.stack_for_longjmp(lr, fp);
     let other_stack = env.stack_for_longjmp(buf.lr, buf.fp);
-    if cur_stack.last() != other_stack.last() {
-        panic!(
-            "longjmp across host stack frames, current {:?}, other {:?}",
-            cur_stack, other_stack
-        );
+    if cur_stack.last().unwrap() != other_stack.last().unwrap() {
+        unimplemented!("Attempted a longjump across host stack frames");
     }
-    let regs = env.cpu.regs_mut();
-    regs[0] = status;
-    regs[4] = buf.r4;
-    regs[5] = buf.r5;
-    regs[6] = buf.r6;
-    regs[abi::FRAME_POINTER] = buf.fp;
-    regs[8] = buf.r8;
-    regs[10] = buf.r10;
-    regs[11] = buf.r11;
-    regs[crate::cpu::Cpu::SP] = buf.sp;
-    regs[crate::cpu::Cpu::LR] = buf.lr;
+    env.cpu.regs_mut()[0] = status;
+    env.cpu.regs_mut()[4] = buf.r4;
+    env.cpu.regs_mut()[5] = buf.r5;
+    env.cpu.regs_mut()[6] = buf.r6;
+    env.cpu.regs_mut()[abi::FRAME_POINTER] = buf.fp;
+    env.cpu.regs_mut()[8] = buf.r8;
+    env.cpu.regs_mut()[10] = buf.r10;
+    env.cpu.regs_mut()[11] = buf.r11;
+    env.cpu.regs_mut()[crate::cpu::Cpu::SP] = buf.sp;
+    env.cpu.regs_mut()[crate::cpu::Cpu::LR] = buf.lr;
     env.cpu
         .branch(GuestFunction::from_addr_with_thumb_bit(buf.lr));
 }
