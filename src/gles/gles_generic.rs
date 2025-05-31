5b7788b9fdde55f7bbc27aa9c96ed0d65fe44f27
dopipo <emssoig@gmail.com>
2025-05-31 20:31:04 +0300

    Update gles_generic.rs

--- src/gles/gles_generic.rs
+++ src/gles/gles_generic.rs
@@ -54,6 +54,7 @@ pub trait GLES {
     unsafe fn GetTexEnviv(&mut self, target: GLenum, pname: GLenum, params: *mut GLint);
     unsafe fn GetPointerv(&mut self, pname: GLenum, params: *mut *const GLvoid);
     unsafe fn Hint(&mut self, target: GLenum, mode: GLenum);
+    unsafe fn Finish(&mut self);
     unsafe fn Flush(&mut self);
     unsafe fn GetString(&mut self, name: GLenum) -> *const GLubyte;
 
