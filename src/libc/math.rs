f934b99ad07d763999bb44ccb6cac273a25530b7
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2025-06-15 08:44:03 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -1027,6 +1027,9 @@ fn CFURLCreateDataAndPropertiesFromResource(_env: &mut Environment, arg1: f32, a
 fn CFURLCreateWithString(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
+fn CFURLCreateStringByAddingPercentEscapes(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
+    arg1.min(arg2)
+}
 fn MFMailComposeErrorDomain(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
@@ -1321,6 +1324,7 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(CFBundleCopyExecutableURL(_, _)),
     export_c_func!(CFURLCreateDataAndPropertiesFromResource(_, _)),
     export_c_func!(CFURLCreateWithString(_, _)),
+    export_c_func!(CFURLCreateStringByAddingPercentEscapes(_, _)),
     export_c_func!(MFMailComposeErrorDomain(_, _)),
     export_c_func!(lstat(_, _)),
     export_c_func!(clock_get_time(_, _)),
