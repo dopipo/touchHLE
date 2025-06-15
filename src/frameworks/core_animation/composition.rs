a657e9257b2828f5e286ed18c474b5b2232b5cd1
dopipo <emssoig@gmail.com>
2025-06-15 22:38:30 +0300

    Update composition.rs

--- src/frameworks/core_animation/composition.rs
+++ src/frameworks/core_animation/composition.rs
@@ -177,21 +177,6 @@ pub fn recomposite_if_necessary(env: &mut Environment, force: bool) -> Option<In
                 gles11::LINEAR as _,
             );
 
-            // iOS and PowerVR only support NPOT texture with CLAMP_TO_EDGE
-            // It doesn't matter for texture data from an image, we don't
-            // want to deal with striping from NPOT to POT conversion.
-            gles.TexParameteri(
-                gles11::TEXTURE_2D,
-                gles11::TEXTURE_WRAP_S,
-                gles11::CLAMP_TO_EDGE as _,
-            );
-
-            gles.TexParameteri(
-                gles11::TEXTURE_2D,
-                gles11::TEXTURE_WRAP_T,
-                gles11::CLAMP_TO_EDGE as _,
-            );
-            
             gles.GenFramebuffersOES(1, &mut framebuffer);
             gles.BindFramebufferOES(gles11::FRAMEBUFFER_OES, framebuffer);
             gles.FramebufferTexture2DOES(
@@ -272,7 +257,6 @@ pub fn recomposite_if_necessary(env: &mut Environment, force: bool) -> Option<In
             present_frame_args.0,
             present_frame_args.1,
             present_frame_args.2,
-                        None,
         );
     }
     env.window().swap_window();
@@ -495,19 +479,5 @@ unsafe fn upload_rgba8_pixels(gles: &mut dyn GLES, pixels: &[u8], dimensions: (u
         gles11::TEXTURE_2D,
         gles11::TEXTURE_MAG_FILTER,
         gles11::LINEAR as _,
-    );
-
-    // iOS and PowerVR only support NPOT texture with CLAMP_TO_EDGE
-    // It doesn't matter for texture data from an image, we don't
-    // want to deal with striping from NPOT to POT conversion.
-    gles.TexParameteri(
-        gles11::TEXTURE_2D,
-        gles11::TEXTURE_WRAP_S,
-        gles11::CLAMP_TO_EDGE as _,
-    );
-    gles.TexParameteri(
-        gles11::TEXTURE_2D,
-        gles11::TEXTURE_WRAP_T,
-        gles11::CLAMP_TO_EDGE as _,
     );
 }
