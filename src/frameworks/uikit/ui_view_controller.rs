0236ef90ddc372ec74fb0e0b9f287ff8e2ffcf7d
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2025-06-15 09:43:22 +0200

    Update ui_view_controller.rs

--- src/frameworks/uikit/ui_view_controller.rs
+++ src/frameworks/uikit/ui_view_controller.rs
@@ -149,6 +149,10 @@ pub const CLASSES: ClassExports = objc_classes! {
     }
 }
 
+- (())setWantsFullScreenLayout:(bool)layout {
+    log!("TODO: setWantsFullScreenLayout:{}", layout);
+}
+
 // Usually overridden by the application
 - (())viewDidLoad {
     log_dbg!("[(UIViewController*){:?} viewDidLoad]", this);
