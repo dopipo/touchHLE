9d8a31cec773770ddec63cf39d86e4b42d8ae482
dopipo <emssoig@gmail.com>
2025-06-22 16:55:09 +0300

    Update audio.rs

--- src/audio.rs
+++ src/audio.rs
@@ -34,6 +34,7 @@ pub enum AudioFormat {
         is_little_endian: bool,
     },
     AppleIma4,
+    Mpeg4Aac,
 }
 /// Fields have the same meanings as in the Core Audio Format's
 /// Audio Description chunk, which is in turn similar to Core Audio Types'
@@ -156,6 +157,9 @@ impl AudioFile {
                         caf::FormatType::AppleIma4 => {
                             assert!(format_flags == 0);
                             AudioFormat::AppleIma4
+                        }
+                        caf::FormatType::Mpeg4Aac => {
+                            AudioFormat::Mpeg4Aac
                         }
                         //
                         // We should expose all of the formats eventually, but
@@ -323,6 +327,7 @@ impl AudioFile {
                 buffer[..bytes_to_read].copy_from_slice(bytes);
                 Ok(bytes_to_read)
             }
+            AudioFormat::Mpeg4Aac => todo!()
         }
     }
-    }
+}
