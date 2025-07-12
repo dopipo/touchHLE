5ba2740599cac95927fe0d3ac1dafe6d705049d8
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2025-07-12 22:40:44 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -1102,6 +1102,9 @@ fn wcsftime(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
 fn pthread_exit(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
     arg1.min(arg2)
 }
+fn ExtAudioFileRead(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
+    arg1.min(arg2)
+}
 
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(abs(_)),
@@ -1395,4 +1398,5 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(glLogicOp(_, _)),
     export_c_func!(wcsftime(_, _)),
     export_c_func!(pthread_exit(_, _)),
+    export_c_func!(ExtAudioFileRead(_, _)),
 ];
