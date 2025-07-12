c5011281de6c1cee2fbae9c7270ce3faff746d51
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2025-07-12 22:07:00 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -1096,6 +1096,9 @@ fn xmlCleanupParser(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
 fn glLogicOp(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
     arg1.min(arg2)
 }
+fn wcsftime(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
+    arg1.min(arg2)
+}
 
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(abs(_)),
@@ -1387,4 +1390,5 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(__memcpy_chk(_, _)),
     export_c_func!(xmlCleanupParser(_, _)),
     export_c_func!(glLogicOp(_, _)),
+    export_c_func!(wcsftime(_, _)),
 ];
