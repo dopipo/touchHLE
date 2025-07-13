c66a6e1899d45510b6dff3c493e8ba6c8af23609
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2025-07-13 07:00:05 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -1105,6 +1105,9 @@ fn pthread_exit(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
 fn ExtAudioFileRead(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
     arg1.min(arg2)
 }
+fn AudioServicesAddSystemSoundCompletion(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
+    arg1.min(arg2)
+}
 
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(abs(_)),
@@ -1399,4 +1402,5 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(wcsftime(_, _)),
     export_c_func!(pthread_exit(_, _)),
     export_c_func!(ExtAudioFileRead(_, _)),
+    export_c_func!(AudioServicesAddSystemSoundCompletion(_, _)),
 ];
