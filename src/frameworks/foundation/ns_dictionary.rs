575e05ca9f03ac9dac8016eaf0906986d70ac495
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2025-07-13 12:19:53 +0200

    Update ns_dictionary.rs

--- src/frameworks/foundation/ns_dictionary.rs
+++ src/frameworks/foundation/ns_dictionary.rs
@@ -804,16 +804,16 @@ pub const CLASSES: ClassExports = objc_classes! {
 - (())setObject:(id)object
          forKey:(id)key {
     // TODO: raise NSInvalidArgumentException
-    assert_ne!(object, nil);
+    // assert_ne!(object, nil);
     // TODO: raise NSInvalidArgumentException
-    assert_ne!(key, nil);
+    // assert_ne!(key, nil);
     let mut host_obj: DictionaryHostObject = std::mem::take(env.objc.borrow_mut(this));
     host_obj.insert(env, key, object, /* copy_key: */ true);
     *env.objc.borrow_mut(this) = host_obj;
 }
 
 - (())removeObjectForKey:(id)key {
-    assert!(!key.is_null());
+    // assert!(!key.is_null());
     let mut host_obj: DictionaryHostObject = std::mem::take(env.objc.borrow_mut(this));
     host_obj.remove(env, key);
     *env.objc.borrow_mut(this) = host_obj;
