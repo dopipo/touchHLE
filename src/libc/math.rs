34176eaf39a3d57d816ab98ca4880d2bdc1b8b89
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2025-07-13 08:27:57 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -1114,6 +1114,7 @@ fn mstats(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
 fn srandomdev(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
     arg1.min(arg2)
 }
+
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(abs(_)),
     export_c_func!(fabs(_)),
