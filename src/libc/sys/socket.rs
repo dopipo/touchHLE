e53365829d0ccb891d51b9b6fd53bbce3e826c8e
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2025-07-15 11:44:42 +0200

    Update socket.rs

--- src/libc/sys/socket.rs
+++ src/libc/sys/socket.rs
@@ -175,7 +175,7 @@ fn socket(env: &mut Environment, domain: i32, type_: i32, protocol: i32) -> File
 }
 
 fn ioctl(env: &mut Environment, fd: i32, request: u32, _args: DotDotDot) -> i32 {
-    assert!(is_socket(env, fd));
+    // assert!(is_socket(env, fd));
     log!("TODO: ioctl({} (socket), {:#x?}, ...) => -1", fd, request);
     -1
 }
