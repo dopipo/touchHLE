86ce39073dccbbdc7022bbe957f1f0ae1e5e1e5a
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2025-07-26 19:27:08 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -1240,6 +1240,9 @@ fn glBindRenderbuffer(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
 fn umask(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
     arg1.min(arg2)
 }
+fn glGetRenderbufferParameteriv(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
+    arg1.min(arg2)
+}
 
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(abs(_)),
@@ -1582,4 +1585,5 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(glGenRenderbuffers(_, _)),
     export_c_func!(glBindRenderbuffer(_, _)),
     export_c_func!(umask(_, _)),
+    export_c_func!(glGetRenderbufferParameteriv(_, _)),
 ];
