8a126ad0cecafeccee8c97d2971379b225fb7014
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2025-07-26 07:49:41 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -1180,6 +1180,9 @@ fn CFUUIDCreate(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
 fn freopen(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
     arg1.min(arg2)
 }
+fn CFUUIDCreateString(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
+    arg1.min(arg2)
+}
 
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(abs(_)),
@@ -1502,4 +1505,5 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(SCNetworkReachabilityUnscheduleFromRunLoop(_, _)),
     export_c_func!(CFUUIDCreate(_, _)),
     export_c_func!(freopen(_, _)),
+    export_c_func!(CFUUIDCreateString(_, _)),
 ];
