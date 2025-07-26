7518ead03107913c8ef837f4d89cff3c29afd539
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2025-07-26 11:20:16 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -1192,6 +1192,9 @@ fn xmlReadFile(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
 fn xmlFreeDoc(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
     arg1.min(arg2)
 }
+fn xmlGetProp(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
+    arg1.min(arg2)
+}
 
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(abs(_)),
@@ -1518,4 +1521,5 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(sqlite3_column_int64(_, _)),
     export_c_func!(xmlReadFile(_, _)),
     export_c_func!(xmlFreeDoc(_, _)),
+    export_c_func!(xmlGetProp(_, _)),
 ];
