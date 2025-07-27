396dc7dca89a44cdc766910ff2cf7838edcc1af2
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2025-07-27 14:58:32 +0200

    Update printf.rs

--- src/libc/stdio/printf.rs
+++ src/libc/stdio/printf.rs
@@ -869,6 +869,16 @@ fn sscanf_common(
                     matched_args -= 1;
                 }
             }
+            b'u' => {
+                // assert!(length_modifier.is_none());
+                let ptr: MutVoidPtr = args.next(env);
+                res.extend_from_slice(format!("{:?}", ptr).as_bytes());
+            }
+            b'c' => {
+                // assert!(length_modifier.is_none());
+                let ptr: MutVoidPtr = args.next(env);
+                res.extend_from_slice(format!("{:?}", ptr).as_bytes());
+            }
             b's' => {
                 // assert_eq!(max_width, 0);
                 // assert!(length_modifier.is_none());
