b9ed87d1d27ba943429d140f9fa90d9c45f55c5e
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2025-07-27 09:39:59 +0200

    Update ns_url_request.rs

--- src/frameworks/foundation/ns_url_request.rs
+++ src/frameworks/foundation/ns_url_request.rs
@@ -8,7 +8,7 @@
 use super::{NSTimeInterval, NSInteger, NSUInteger};
 use crate::frameworks::foundation::ns_string::to_rust_string;
 use crate::msg;
-use crate::objc::{autorelease, id, msg, nil, objc_classes, release, ClassExports};
+use crate::objc::{autorelease, id, nil, objc_classes, release, ClassExports};
 
 type NSURLRequestCachePolicy = NSUInteger;
 const NSURLRequestUseProtocolCachePolicy: NSURLRequestCachePolicy = 0;
