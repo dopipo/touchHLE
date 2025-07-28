fb1ca2f30660368d3ceb2fa19e07a3d2998d553e
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2025-07-28 21:13:11 +0200

    Update ui_window.rs

--- src/frameworks/uikit/ui_view/ui_window.rs
+++ src/frameworks/uikit/ui_view/ui_window.rs
@@ -104,6 +104,10 @@ pub const CLASSES: ClassExports = objc_classes! {
     }
 }
 
+- (id)contentMode {
+    nil
+}
+
 - (())makeKeyAndVisible {
     // TODO: We don't currently have send any non-touch events to windows,
     // so there's no meaning in it yet.
