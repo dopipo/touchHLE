8e1d332d80c5007d6bb4e3d6a1d8a3588b5596ab
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2025-07-28 14:34:14 +0200

    Update audio_file.rs

--- src/frameworks/audio_toolbox/audio_file.rs
+++ src/frameworks/audio_toolbox/audio_file.rs
@@ -84,7 +84,7 @@ pub fn AudioFileOpenURL(
     // Hints for well-known types are ignored as well.
     assert!(in_file_type_hint == 0);
     match in_file_type_hint {
-    audio_file_open_inner(env, in_file_ref, out_audio_file)
+    audio_file_open_inner(env, in_file_ref, out_audio_file),
         0 => {}
 }
         kAudioFileCAFType => {
