b1bc850f377b9e4674c67bd62fae6df9ae45c998
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2025-07-28 07:52:42 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -1315,6 +1315,9 @@ fn xmlStrcmp(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
 fn xmlNextElementSibling(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
     arg1.min(arg2)
 }
+fn glob(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
+    arg1.min(arg2)
+}
 
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(abs(_)),
@@ -1682,4 +1685,5 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(CGContextSetBlendMode(_, _)),
     export_c_func!(xmlStrcmp(_, _)),
     export_c_func!(xmlNextElementSibling(_, _)),
+    export_c_func!(glob(_, _)),
 ];
