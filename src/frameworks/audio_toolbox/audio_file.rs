3bc72fca34a027c900ac723b0e2ef18eff9741e7
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2025-07-28 15:07:58 +0200

    Update audio_file.rs

--- src/frameworks/audio_toolbox/audio_file.rs
+++ src/frameworks/audio_toolbox/audio_file.rs
@@ -219,7 +219,7 @@ pub fn AudioFileOpenWithCallbacks(
     };
     let guest_audio_file = env.mem.alloc_and_write(OpaqueAudioFileID { _filler: 0 });
 
-    let host_object = AudioFileHostObject { audio_file };
+    let host_object = AudioFileHostObject { audio_file, position };
 
     State::get(&mut env.framework_state)
         .audio_files
