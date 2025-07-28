c44115cb1112d75f4718948b43bf7d2292f018fb
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2025-07-28 12:19:47 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -1324,6 +1324,9 @@ fn EAGLGetVersion(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
 fn pthread_cond_broadcast(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
     arg1.min(arg2)
 }
+fn sigaltstack(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
+    arg1.min(arg2)
+}
 
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(abs(_)),
@@ -1694,4 +1697,5 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(glob(_, _)),
     export_c_func!(EAGLGetVersion(_, _)),
     export_c_func!(pthread_cond_broadcast(_, _)),
+    export_c_func!(sigaltstack(_, _)),
 ];
