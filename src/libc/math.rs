d3194a9e34eab97fe852e4a40ef2978b854c55c9
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2025-07-29 09:53:17 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -1324,6 +1324,12 @@ fn CATransform3DMakeScale(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
 fn pthread_attr_getschedparam(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
     arg1.min(arg2)
 }
+fn sched_get_priority_min(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
+    arg1.min(arg2)
+}
+fn sched_get_priority_max(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
+    arg1.min(arg2)
+}
 
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(abs(_)),
@@ -1694,4 +1700,6 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(CATransform3DConcat(_, _)),
     export_c_func!(CATransform3DMakeScale(_, _)),
     export_c_func!(pthread_attr_getschedparam(_, _)),
+    export_c_func!(sched_get_priority_min(_, _)),
+    export_c_func!(sched_get_priority_max(_, _)),
 ];
