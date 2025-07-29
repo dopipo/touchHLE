3804b9c98ff488ae424bee60f57a3bd7b089f609
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2025-07-29 08:39:23 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -1318,6 +1318,9 @@ fn sigaltstack(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
 fn CATransform3DConcat(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
     arg1.min(arg2)
 }
+fn CATransform3DMakeScale(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
+    arg1.min(arg2)
+}
 
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(abs(_)),
@@ -1686,4 +1689,5 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(pthread_cond_broadcast(_, _)),
     export_c_func!(sigaltstack(_, _)),
     export_c_func!(CATransform3DConcat(_, _)),
+    export_c_func!(CATransform3DMakeScale(_, _)),
 ];
