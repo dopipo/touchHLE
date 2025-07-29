3cb2e5f3703d11e00a2de4e6695b1c5c4807acdf
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2025-07-29 22:35:05 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -1372,6 +1372,9 @@ fn OSAtomicCompareAndSwapIntBarrier(_env: &mut Environment, arg1: f64, arg2: f64
 fn __assert_rtn(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
     arg1.min(arg2)
 }
+fn OSAtomicAdd32(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
+    arg1.min(arg2)
+}
 
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(abs(_)),
@@ -1758,4 +1761,5 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(method_setImplementation(_, _)),
     export_c_func!(OSAtomicCompareAndSwapIntBarrier(_, _)),
     export_c_func!(__assert_rtn(_, _)),
+    export_c_func!(OSAtomicAdd32(_, _)),
 ];
