99715f1bfb85d0cef31dd26e2c7f30ebfb6d7504
dopipo <emssoig@gmail.com>
2025-08-15 17:07:31 +0300

    Update ns_lock.rs

--- src/frameworks/foundation/ns_lock.rs
+++ src/frameworks/foundation/ns_lock.rs
@@ -53,11 +53,20 @@ pub const CLASSES: ClassExports = objc_classes! {
     if let Some(locked_by_thread) = host_object.locked_by {
         assert!(locked_by_thread == env.current_thread);
     } else {
-        echo!("*** -[NSLock unlock]: lock (<NSLock: {:?}> '{:?}') unlocked when not locked", this, host_object.name);
+        echo!("*** -[NSLock unlock]: lock (<NSLock: {:?}> '{:?}') unlocked when not locked", t
+              his, host_object.name);
     }
     assert!(pthread_mutex_unlock(env, host_object.pthread_mutex_ptr) == 0);
     env.objc.borrow_mut::<NSLockHostObject>(this).locked_by = None
 }
+
+- (bool)tryLock {
+    let host_object = env.objc.borrow::<NSLockHostObject>(this);
+    if env.mutex_state.mutex_is_locked(host_object.mutex_id) {
+        false
+    } else {
+        env.lock_mutex(host_object.mutex_id).is_ok()
+    }
 
 - (())setName:(id)name { // NSString *
     // @property(copy), name has to be copied
