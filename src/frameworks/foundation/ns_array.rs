24d94083e7685530889d14844d6a028109947def
dopipo <emssoig@gmail.com>
2025-08-20 17:44:06 +0300

    Update ns_array.rs

--- src/frameworks/foundation/ns_array.rs
+++ src/frameworks/foundation/ns_array.rs
@@ -7,7 +7,7 @@
 
 use super::ns_enumerator::{fast_enumeration_helper, NSFastEnumerationState};
 use super::ns_property_list_serialization::deserialize_plist_from_file;
-use super::{ns_keyed_unarchiver, ns_string, ns_url, NSNotFound, NSOrderedAscending, NSOrderedDescending, NSOrderedSame, NSInteger, NSUInteger};
+use super::{ns_keyed_unarchiver, ns_string, ns_url, NSNotFound, NSRange, NSOrderedAscending, NSOrderedDescending, NSOrderedSame, NSInteger, NSUInteger};
 use crate::abi::{CallFromHost, GuestFunction};
 use crate::fs::GuestPath;
 use crate::mem::{MutPtr, MutVoidPtr};
@@ -405,6 +405,18 @@ pub const CLASSES: ClassExports = objc_classes! {
     build_description(env, this)
 }
 
+- (id)subarrayWithRange:(NSRange)range {
+    let mut tmp = Vec::new();
+    tmp.extend_from_slice(
+        &env.objc.borrow::<ArrayHostObject>(this).array[range.location as usize..(range.location + range.length) as usize]
+    );
+    for &obj in &tmp {
+        retain(env, obj);
+    }
+    let res = from_vec(env, tmp);
+    autorelease(env, res)
+}
+    
 @end
 
 // Special variant for use by CFArray with NULL callbacks: objects aren't
